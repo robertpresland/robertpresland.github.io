5dcef71466d56594a5d70bfb4d5e58af849d4928
robertpresland <rob@wide-sky.co.uk>
2025-12-07 22:46:10 +0000

    stbarts

---
+++ js/jquery/jsound.js
@@ -0,0 +1,9 @@
+(function($){
+
+  $.extend({
+    playSound: function(){
+      return $("<embed src='"+arguments[0]+"' hidden='true' autostart='true' loop='false' class='playSound'>").appendTo('body');
+    }
+  });
+
+})(jQuery);
